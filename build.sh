905996ddd84d11b04055f4d18328bdefe0ca4a3d
Skibidi toilet targan <128474370+handingslider@users.noreply.github.com>
2025-08-16 23:20:04 +0300

    Made it buildable(probably)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-# ROM Build commands for Mi 11 Lite 5G renoir
+# ROM Build commands for Poco X6 Pro 5G Duchamp
 
 # Remove local_manifests
 
@@ -13,21 +13,21 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 # Remove old device specific repos
 rm -rf device/xiaomi/duchamp
 rm -rf vendor/xiaomi/duchamp
-rm -rf kernel/xiaomi/duchamp
+rm -rf kernel/xiaomi/mt6897
 rm -rf hardware/xiaomi
+rm -rf device/mediatek/sepolicy_vndr
+rm -rf device/xiaomi/duchamp-kernel
+rm -rf hardware/mediatek
 
 # Clone new device specific repos
-
-# Device configuration
-git clone https://github.com/mt6897-devs/device_xiaomi_duchamp.git device/xiaomi/duchamp
-
-# Vendor tree
+git clone https://github.com/mt6897-devs/device_xiaomi_duchamp device/xiaomi/duchamp 
+git clone https://github.com/mt6897-devs/device_xiaomi_duchamp-kernel device/xiaomi/duchamp-kernel
 git clone https://github.com/mt6897-devs/vendor_xiaomi_duchamp vendor/xiaomi/duchamp
 
-# Kernel Tree
-git clone https://github.com/mt6897-devs/android_kernel_6.1 kernel/xiaomi/duchamp
+git clone https://github.com/mt6897-devs/android_kernel_6.1 kernel/xiaomi/mt6897 --depth=1
 
-# Hardware
+git clone https://github.com/mt6897-devs/device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+git clone https://github.com/mt6897-devs/hardware_mediatek hardware/mediatek
 git clone https://github.com/mt6897-devs/hardware_xiaomi hardware/xiaomi
 
 . build/envsetup.sh
